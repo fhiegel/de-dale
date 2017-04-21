0bd23cb84d9388c321bc8a902c48699167bcaf5b
fhiegel <fabien.hiegel@gmail.com>
2017-04-21 18:11:30 +0200

    Dice | Resources | Integration
    Add simple resource configuration for StringCalculator.

--- src/main/java/com/dedale/DeDaleResourceConfig.java
+++ src/main/java/com/dedale/DeDaleResourceConfig.java
@@ -1,11 +1,22 @@
 package com.dedale;
 
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
+
+import com.dedale.calculator.StringCalculator;
+import com.dedale.calculator.StringCalculatorInputValidator;
 
 public class DeDaleResourceConfig extends ResourceConfig {
     
     public DeDaleResourceConfig() {
         packages("com.dedale.api");
+        register(new AbstractBinder() {
+            @Override
+            protected void configure() {
+                bind(StringCalculator.class).to(StringCalculator.class);
+                bind(StringCalculatorInputValidator.class).to(StringCalculatorInputValidator.class);
+            }
+        });
     }
     
 }
