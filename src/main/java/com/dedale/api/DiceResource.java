32c2fa8eb7dfbb0dd8cf962468cc3d0ea7936374
fhiegel <fabien.hiegel@gmail.com>
2017-04-21 10:30:04 +0200

    Dice | Resources | Initialization
    Add base resources for throwing a dice.

---
+++ src/main/java/com/dedale/api/DiceResource.java
@@ -0,0 +1,26 @@
+package com.dedale.api;
+
+import javax.inject.Inject;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("dices")
+public class DiceResource {
+    
+    @Inject
+    private ResourceValidator validator;
+    
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public String defaultRoll(String diceSentence) {
+        if(!validator.validate(diceSentence)){
+            throw new WebApplicationException("Cannot parse dice sentence. sentence:"+diceSentence, Response.Status.BAD_REQUEST);
+        }
+        return diceSentence;
+    }
+    
+}
---
+++ src/main/java/com/dedale/api/ResourceValidator.java
@@ -0,0 +1,7 @@
+package com.dedale.api;
+
+public interface ResourceValidator {
+    
+    boolean validate(String input);
+    
+}
