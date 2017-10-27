40c75f907538ad1963645476eded2a02c4173064
fhiegel <fabien.hiegel@gmail.com>
2017-10-27 13:28:18 +0200

    Slack | Application | Restore deprecated API

--- src/main/java/com/dedale/api/DiceResource.java
+++ src/main/java/com/dedale/api/DiceResource.java
@@ -1,5 +1,8 @@
 package com.dedale.api;
 
+import static com.dedale.markdown.MarkdownTags.MARKDOWN_BOLD;
+import static com.dedale.markdown.MarkdownTags.MARKDOWN_CODE;
+
 import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -10,6 +13,9 @@ import javax.ws.rs.core.Response;
 
 import com.dedale.calculator.StringCalculator;
 import com.dedale.calculator.StringCalculatorInputValidator;
+import com.dedale.slack.client.request.SlackRequest;
+import com.dedale.slack.client.response.legacy.SlackResponse;
+import com.dedale.slack.client.response.legacy.SlackResponseBuilder;
 
 @Path("dices")
 public class DiceResource {
@@ -24,10 +30,43 @@ public class DiceResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String defaultRoll(String diceSentence) {
         if (!validator.validate(diceSentence)) {
-            throw new WebApplicationException("Cannot parse dice sentence. sentence:" + diceSentence,
-                    Response.Status.BAD_REQUEST);
+            throw new WebApplicationException("Cannot parse dice sentence. sentence:" + diceSentence, Response.Status.BAD_REQUEST);
         }
         return calculator.calculate(diceSentence).toString();
+    }
+
+    @POST
+    @Path("/slack")
+    @Deprecated
+    public SlackResponse slackRoll(SlackRequest slackRequest) {
+        String diceSentence = slackRequest.getText();
+        if (!validator.validate(diceSentence)) {
+            String responseText = "Impossible de lancer la définition de dé suivante : " + MARKDOWN_CODE + slackRequest.getText()
+                    + MARKDOWN_CODE;
+            return SlackResponseBuilder
+                    .beginResponse()
+                    .inChannel()
+                    .addAttachment()
+                    .withColor("#ff0000")
+                    .withAuthorName(slackRequest.getUserName())
+                    .withText(responseText)
+                    .markdownInText()
+                    .endAttachment()
+                    .build();
+        }
+        String diceResult = calculator.calculate(diceSentence).toString();
+
+        String responseText = MARKDOWN_BOLD + slackRequest.getText() + "=" + MARKDOWN_BOLD + " " + diceResult;
+
+        return SlackResponseBuilder
+                .beginResponse()
+                .inChannel()
+                .addAttachment()
+                .withAuthorName(slackRequest.getUserName())
+                .withText(responseText)
+                .markdownInText()
+                .endAttachment()
+                .build();
     }
 
 }
